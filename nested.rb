bf6e30e2d5791a57b8f045aaa084b46fcfc350e7
Eric <contreras.eric18@gmail.com>
2021-01-21 15:59:44 -0500

    commit tree 088aa78571db773008cc25510aebfd58c5f2ddad

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,14 +56,17 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    lang = programmer_hash[:dennis_ritchie][:languages]
+    return lang[0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+hash = {:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+}
 
 	programmer_hash = 
  		{
@@ -79,8 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+#merges the two hashes, hash to the top of programer_hash
+programmer_hash.merge(hash, programmer_hash)    
 end
 
 def changing_alan
@@ -101,8 +105,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     #replace hash value with new one
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+return programmer_hash
 end
